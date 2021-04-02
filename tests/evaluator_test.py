e4ef1e027fe38474bc6bfe0480ac6298568728e8
jorge.vidoni <jorge.vidoni@MAC-020.local>
2021-04-02 09:59:09 -0300

    Se agrega evaluacion de infix

--- tests/evaluator_test.py
+++ tests/evaluator_test.py
@@ -35,7 +35,23 @@ class EvaluatorTest(TestCase):
     def test_boolean_evaluation(self) -> None:
         tests: List[Tuple[str,bool]] = [
             ('verdadero',True),
-            ('falso',False),
+            ('falso', False),
+            ('1 < 2', True),
+            ('1 > 2', False),
+            ('1 < 1', False),
+            ('1 > 1', False),
+            ('1 == 1', True),
+            ('1 != 1', False),
+            ('1 != 2', True),
+            ('verdadero == verdadero', True),
+            ('falso == falso', True),
+            ('verdadero == falso', False),
+            ('verdadero != falso', True),
+            ('(1 < 2) == verdadero', True),
+            ('(1 < 2) == falso', False),
+            ('(1 > 2) == verdadero', False),
+            ('(1 > 2) == falso', True),
+            
         ]
 
         for source, expected in tests:
@@ -49,21 +65,20 @@ class EvaluatorTest(TestCase):
             ('10',10),
             ('-5',-5),
             ('-10',-10),
+            ('5 + 5',10),
+            ('5 - 10',-5),
+            ('2 * 2 * 2 * 2',16),
+            ('2 * 5 - 3',7),
+            ('50 / 2',25),
+            ('2 * (5 - 3 )',4),
+            ('(2 + 7 ) / 3',3),
+            ('50 / 2 * 2 + 10',60),
+            ('5 / 2 ',2),
         ]
 
         for source, expected in tests:
             evaluated = self._evaluate_test(source)
             self._test_integer_object(evaluated, expected)
-
-    def _evaluate_test(self, source:str) -> Object:
-        lexer: Lexer = Lexer(source)
-        parser: Parser = Parser(lexer)
-        program: Program = parser.parse_program()
-
-        evaluated = evaluate(program)
-
-        assert evaluated is not None
-        return evaluated
 
     def _test_boolean_object(self, evaluated: Object, expected: bool) -> None:
         assert evaluated is not None
@@ -76,7 +91,16 @@ class EvaluatorTest(TestCase):
 
         evaluated = cast(Integer, evaluated)
         self.assertEquals(evaluated.value, expected)
+    
+    def _evaluate_test(self, source:str) -> Object:
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+        program: Program = parser.parse_program()
 
+        evaluated = evaluate(program)
+
+        assert evaluated is not None
+        return evaluated
 
 
 
